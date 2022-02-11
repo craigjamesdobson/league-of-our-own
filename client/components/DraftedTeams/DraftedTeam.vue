27a8b52766e24becdb95fb20e6d35c47d74f85fc
Craig Dobson <dobson.cj@gmail.com>
2022-02-11 21:01:59 +0000

    Fix issue with multiple transferred players  display

--- client/components/DraftedTeams/DraftedTeam.vue
+++ client/components/DraftedTeams/DraftedTeam.vue
@@ -28,19 +28,19 @@
       :key="player.playerName"
       class="relative text-sm"
       :class="{
-        'transferred-player': player.transfers.length
+        'transferred-player': player.transfers.length,
       }"
     >
       <div
         v-if="
           !player.transfers.length ||
-          player.transfers[0].transferWeek > fixtureWeek
+          player.transfers.at(-1).transferWeek > fixtureWeek
         "
         class="flex items-center w-full border-b border-gray-100"
         :class="{
           'opacity-25': player.isUnavailableForSeason,
           'bg-red-500 text-white':
-            dynamicView && getPlayerGameweekData(player).sentOff
+            dynamicView && getPlayerGameweekData(player).sentOff,
         }"
       >
         <span class="w-1/12 p-2">{{ player.id }}</span>
@@ -66,44 +66,44 @@
         :key="player.transfers[0].player.id"
         class="flex items-center w-full border-b border-gray-100 cursor-pointer"
         :class="
-              player.transfers[0].isCurrentWeekTransfer || player.transfers[0].transferWeek === fixtureWeek ? 'bg-yellow-300 text-gray-800' : 'bg-green-500 text-white',
+              player.transfers.at(-1).isCurrentWeekTransfer || player.transfers.at(-1).transferWeek === fixtureWeek ? 'bg-yellow-300 text-gray-800' : 'bg-green-500 text-white',
             "
       >
-        <span class="w-1/12 p-2">{{ player.transfers[0].player.id }}</span>
+        <span class="w-1/12 p-2">{{ player.transfers.at(-1).player.id }}</span>
         <span class="w-2/12 p-2">
           <img
             class="w-6 h-6 m-auto rounded-full shadow-md"
-            :src="player.transfers[0].player.image"
-            :alt="player.transfers[0].player.name"
+            :src="player.transfers.at(-1).player.image"
+            :alt="player.transfers.at(-1).player.name"
             @error="loadFallbackImage"
           />
         </span>
         <span class="w-2/12 p-2">
-          {{ player.transfers[0].player.teamShort }}
+          {{ player.transfers.at(-1).player.teamShort }}
         </span>
         <span class="w-5/12 p-2 text-center">
-          {{ player.transfers[0].player.name }}
+          {{ player.transfers.at(-1).player.name }}
         </span>
         <span v-if="dynamicView" class="w-2/12 p-2 text-center">
-          {{ getPlayerGameweekData(player.transfers[0].player).points }}
+          {{ getPlayerGameweekData(player.transfers.at(-1).player).points }}
         </span>
         <span v-else class="w-2/12 p-2 text-center">
-          {{ player.transfers[0].player.price }}
+          {{ player.transfers.at(-1).player.price }}
         </span>
       </div>
       <div
         v-if="
           !player.transfers.length ||
-          player.transfers[0].transferWeek <= fixtureWeek
+          player.transfers.at(-1).transferWeek <= fixtureWeek
         "
         class="flex flex-wrap justify-between w-full h-full"
         :class="{
           'hidden absolute top-0 z-10 bg-blue-600 text-white old-transfer hover:flex':
-            player.transfers.length
+            player.transfers.length,
         }"
       >
         <div
-          v-if="player.transfers.length"
+          v-if="player.transfers.length === 1"
           class="
             flex
             items-center
@@ -121,6 +121,28 @@
           />
           {{ player.name }} was transferred out on week
           {{ player.transfers[0].transferWeek }}
+        </div>
+        <div
+          v-if="player.transfers.length === 2"
+          class="
+            flex
+            items-center
+            justify-center
+            w-full
+            text-center
+            cursor-pointer
+          "
+        >
+          <img
+            class="w-6 h-6 mr-4 border border-white rounded-full"
+            :src="player.transfers[0].player.image"
+            :alt="player.transfers[0].player.name"
+            @error="loadFallbackImage"
+          />
+          <template v-if="player.transfers.length > 1">
+            {{ player.transfers[0].player.name }} was transferred out on week
+            {{ player.transfers[1].transferWeek }}
+          </template>
         </div>
       </div>
     </div>
@@ -146,7 +168,7 @@ export default {
   props: {
     team: Object,
     fixtureWeek: Number,
-    dynamicView: Boolean
+    dynamicView: Boolean,
   },
 
   setup(props) {
@@ -165,8 +187,8 @@ export default {
     return {
       loadFallbackImage,
       getPlayerGameweekData,
-      getTeamGameweekData
+      getTeamGameweekData,
     }
-  }
+  },
 }
 </script>

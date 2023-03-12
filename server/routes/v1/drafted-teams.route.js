4dd8c10cfb4750eb8e6ed90c6dff32e3b7833d80
Craig Dobson <craig@webselect.net>
2023-03-12 20:46:49 +0000

    flatten sub v2 files to main

--- server/routes/v1/drafted-teams.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const draftedTeamsController = require('../../controllers/drafted-teams.controller');
-
-router.get('/', draftedTeamsController.getDraftedTeams);
-router.post('/update', draftedTeamsController.updateDraftedTeams);
-
-module.exports = router;
--- server/routes/v1/fixtures.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const fixturesController = require('../../controllers/fixtures.controller');
-
-router.get('/', fixturesController.getFixtures);
-
-router.post('/update', fixturesController.updateFixtures);
-
-module.exports = router;

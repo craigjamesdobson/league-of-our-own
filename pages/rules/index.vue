248dbc14a02b3aff3111a9103af08a3e99e83c51
Craig Dobson <dobson.cj@gmail.com>
2023-07-28 19:48:30 +0100

    Fix button alignment

--- pages/rules/index.vue
+++ pages/rules/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="text-center">
+  <div class="flex flex-col flex-wrap text-center">
     <p class="m-5">
       Unable to display PDF file?
       <a
-        class="px-6 py-2 ml-5 text-white rounded-md bg-primary"
+        class="block px-6 py-2 mt-5 ml-5 text-white rounded-md lg:inline-flex bg-primary"
         href="/2023-2024-rules.pdf"
         >Download here</a
       >

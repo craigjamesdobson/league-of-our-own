07ec591748cff16903d095db9dfa5efb1582d691
Craig Dobson <craigjamesdobson.dev@gmail.com>
2024-01-08 20:52:26 +0000

    tidy up skeleton loaders

--- components/Players.vue
+++ components/Players.vue
@@ -20,18 +20,7 @@ const setSelectedPlayerAndQueryParam = (playerID: number) => {
 </script>
 
 <template>
-  <div v-if="!playerStore.isLoaded">
-    <h1 class="main-heading">Loading...</h1>
-    <div class="grid grid-cols-1 gap-3 md:grid-cols-2">
-      <div v-for="i in 50" :key="i" class="flex flex-row gap-5 mb-2">
-        <SkeletonLoader class="w-1/12 h-5" />
-        <SkeletonLoader type="circle" class="w-5 h-5" />
-        <SkeletonLoader class="w-2/12 h-5" />
-        <SkeletonLoader class="w-5/12 h-5" />
-        <SkeletonLoader class="w-2/12 h-5" />
-      </div>
-    </div>
-  </div>
+  <PlayerLoader v-if="!playerStore.isLoaded" />
   <div v-else>
     <PlayerModal v-model="showDialog" :selected-player="selectedPlayer" />
     <div v-for="(data, index) in playerData" :key="index">

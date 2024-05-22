f6e66ccb7153931c3c4d283e9120fae0ded7558d
Craig Dobson <dobson.cj@gmail.com>
2024-05-22 20:56:43 +0100

    Fix issue with tooltip styling

--- presets/aura/tooltip/index.js
+++ presets/aura/tooltip/index.js
@@ -3,7 +3,6 @@ export default {
         class: [
             // Position and Shadows
             'absolute',
-            'shadow-md',
             'p-fadein',
             // Spacing
             {

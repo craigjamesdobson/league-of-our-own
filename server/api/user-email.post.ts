a3560ccc2d05c4ac3f87fc5125e32e4b67a94fed
Craig Dobson <dobson.cj@gmail.com>
2024-08-12 20:20:36 +0100

    Dynamic email title depending on if team is being updated

--- server/api/user-email.post.ts
+++ server/api/user-email.post.ts
@@ -8,7 +8,7 @@ export default defineEventHandler(async (event) => {
     const data = await resend.emails.send({
       from: 'Jim & Craig <leagueofourown@craigjamesdobson.dev>',
       to: [body.email],
-      subject: 'Thank you for team submission',
+      subject: body.title,
       html: body.html
     });
 

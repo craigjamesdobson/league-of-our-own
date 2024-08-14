f6f817e4db16130702aaf5536e948399ff432490
Craig Dobson <dobson.cj@gmail.com>
2024-08-14 19:58:40 +0100

    Remove team entry form and add message

--- pages/team-builder/index.vue
+++ pages/team-builder/index.vue
@@ -1,35 +1,6 @@
 <template>
-  <div class="flex flex-col-reverse gap-5 2xl:flex-row">
-    <Toast position="top-center" />
-    <div class="px-5 2xl:w-96">
-      <h2 class="mb-2.5 text-xl font-black uppercase text-center 2xl:text-left">Team Details</h2>
-      <TeamBuilderForm :drafted-team-data="draftedTeamData" :drafted-team-players="draftedTeamPlayers" />
-    </div>
-    <div class="flex flex-col">
-      <h2 class="mb-2.5 text-xl text-center font-black uppercase">Pick a team</h2>
-      <div class="2xl:hidden text-center">
-        <Message severity="info" :closable="false" v-if="isExistingDraftedTeam">
-          You are editing your existing team. <br /> It was last edited on <strong>{{ new
-            Date(draftedTeamData.updated_at ?? draftedTeamData.created_at).toLocaleDateString('en-GB') }}</strong>
-        </Message>
-        <div class="flex flex-col text-xs" v-else>
-          <Divider />
-          <p class="mb-5">Pick your team, fill in the form below and then submit your team.</p>
-          <p class="mb-5">Once you submit your team you will recieve an email confirming your selection and a link to edit
-            your team if you
-            wish.</p>
-          <Divider />
-        </div>
-        <p class="font-bold text-xs mb-5">If you do not receive an email when submitting or editing your team submission has
-        failed so please email us with as much detail as possible - <a class="underline font-bold"
-          href="mailto:leagueofourown.fpl@gmail.com"> leagueofourown.fpl@gmail.com</a>.</p>
-      </div>
-      <div class="grid grid-cols-12 justify-center gap-5">
-        <PlayerSection v-for="(player, index) in draftedTeamPlayers" :key="index" v-model:player="player.selectedPlayer"
-          :selected-players="selectedPlayerIds" :position="player.position" />
-      </div>
-    </div>
-    <ScrollTop class="!bottom-40 !bg-primary" />
+  <div class="h-full flex justify-center items-center">
+    <Message :closable="false">Team entries are now closed. Teams will be available to view before the season begins</Message>
   </div>
 </template>
 

16e5aeed62cb91206152bab4090891a28833eab5
Craig Dobson <dobson.cj@gmail.com>
2025-01-07 20:38:26 +0000

    Create a dialog with a breakdown of weekly points

---
+++ components/Drafted/DraftedTeamPreviewWithPoints.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import type { DraftedTeam } from '~/types/DraftedTeam';
+import { useFixtureStore } from '~/stores/fixtures';
+import type { DraftedPlayerWithWeeklyStats } from '~/types/DraftedPlayer';
+
+const fixtureStore = useFixtureStore();
+
+const props = defineProps({
+  draftedTeam: {
+    type: Object as PropType<DraftedTeam>,
+    default: null,
+  },
+  activeWeek: {
+    type: Number,
+    required: true,
+  },
+});
+
+const isActiveTransfer = (transferDate: Date) => {
+  return new Date(transferDate) > new Date();
+};
+
+const calculatedWeeklyStats = computed(() => {
+  return props.draftedTeam.players.reduce(
+    (accumulatedStats, player: DraftedPlayerWithWeeklyStats) => {
+      let activePlayer = player.transfers
+        .filter(x => x.transfer_week <= fixtureStore.selectedGameweek)
+        .sort((a, b) => b.transfer_week - a.transfer_week)[0] || player;
+
+      if (player.selected) {
+        activePlayer = player;
+      }
+
+      if (player.transfers.find(transfer => transfer.selected)) {
+        activePlayer = player.transfers.find(transfer => transfer.selected);
+      }
+
+      // Calculate points based on the selected transfer or the usual logic
+      const currentPlayerPoints = activePlayer
+        ? activePlayer.points || 0
+        : player.transfers.reduce((points, transfer) => {
+            return transfer.transfer_week <= fixtureStore.selectedGameweek
+              ? transfer.points
+              : points;
+          }, player.points || 0);
+
+      return {
+        drafted_team_id: props.draftedTeam.drafted_team_id,
+        points: accumulatedStats.points + currentPlayerPoints,
+        goals: accumulatedStats.goals + (activePlayer?.week_goals || 0),
+        assists: accumulatedStats.assists + (activePlayer?.week_assists || 0),
+        red_cards: accumulatedStats.red_cards + (activePlayer?.week_redcards ? 1 : 0),
+        clean_sheets:
+          accumulatedStats.clean_sheets + (activePlayer?.week_cleansheets ? 1 : 0),
+      };
+    },
+    {
+      points: 0,
+      goals: 0,
+      assists: 0,
+      red_cards: 0,
+      clean_sheets: 0,
+    },
+  );
+});
+
+</script>
+
+<template>
+  <div
+    v-if="props.draftedTeam"
+    class="rounded-sm bg-white p-4 pt-0"
+  >
+    <div
+      class="mb-2 flex items-center justify-between border-b border-gray-800 p-2 pt-0"
+      :class="{
+        'bg-red-200': props.draftedTeam?.is_invalid_team,
+      }"
+    >
+    </div>
+    <div
+      v-for="player in props.draftedTeam.players"
+      :key="player.drafted_player_id"
+      class="relative text-sm"
+      :class="{
+        'bg-yellow-200':
+          !!player.transfers.length
+          && isActiveTransfer(player.transfers.at(-1)!.active_transfer_expiry),
+        'bg-green-200':
+          !!player.transfers.length
+          && !isActiveTransfer(player.transfers.at(-1)!.active_transfer_expiry),
+      }"
+    >
+      <div class="flex w-full items-center border-b border-gray-100">
+        <DraftedPlayerWithPoints
+          v-if="!player.transfers.length"
+          :drafted-player="player"
+          :weekly-stats="calculatedWeeklyStats"
+        />
+        <DraftedActivePlayer
+          v-else-if="player.transfers.at(-1) !== null"
+          :active-gameweek="activeWeek"
+          :drafted-player="player"
+          class="w-full"
+        />
+      </div>
+    </div>
+    <div class="flex justify-between px-2.5 pt-2.5">
+      <span>Total</span>
+      <strong>
+        {{ calculatedWeeklyStats.points }}
+      </strong>
+    </div>
+  </div>
+</template>
--- pages/table/index.vue
+++ pages/table/index.vue
@@ -1,17 +1,25 @@
 <script lang="ts" setup>
 import { useTableStore } from '@/stores/table';
+import { useDraftedTeamsStore } from '~/stores/draftedTeams';
 
+const draftedTeamsStore = useDraftedTeamsStore();
 const route = useRoute();
 const router = useRouter();
 const tableStore = useTableStore();
 const weeks = ref(Array.from({ length: 38 }, (_, index) => index + 1));
 const selectedWeek = ref(+route.query.week || 1);
+const selectedDraftedTeam = ref();
+const draftedTeamsWithPoints = ref();
+
 
 watch(selectedWeek, async (newWeek) => {
   if (tableStore.weeklyWinners?.find(x => x.week === newWeek).points !== null) {
     await tableStore.fetchWeeklyStats(newWeek);
-  }
-  else {
+    draftedTeamsWithPoints.value =
+      await draftedTeamsStore.fetchDraftedTeamsWithPlayerPointsByGameweek(
+        newWeek
+      );
+  } else {
     tableStore.weeklyData = [];
   }
   await router.push({
@@ -19,6 +27,14 @@ watch(selectedWeek, async (newWeek) => {
     query: { week: newWeek },
   });
 }, { immediate: true });
+
+const visible = ref(false);
+
+const showTeamPoints = (event: Event, id: number) => {
+  selectedDraftedTeam.value = draftedTeamsWithPoints.value.find(x => x.drafted_team_id === id);
+  visible.value = true;
+};
+
 </script>
 
 <template>
@@ -29,17 +45,9 @@ watch(selectedWeek, async (newWeek) => {
           Week {{ selectedWeek }}
         </h1>
         <div class="mb-5 flex flex-col items-end gap-2.5">
-          <label
-            class="font-bold uppercase"
-            for="gameweeks"
-          >Select a game week</label>
+          <label class="font-bold uppercase" for="gameweeks">Select a game week</label>
           <div class="flex gap-2.5">
-            <Select
-              v-model="selectedWeek"
-              :options="weeks"
-              placeholder="Select a gameweek"
-              scroll-height="25rem"
-            >
+            <Select v-model="selectedWeek" :options="weeks" placeholder="Select a gameweek" scroll-height="25rem">
               <template #value="slotProps">
                 <div class="flex items-center">
                   <div>WEEK {{ slotProps.value }}</div>
@@ -55,20 +63,11 @@ watch(selectedWeek, async (newWeek) => {
         </div>
       </div>
       <div
-        class="lg:hidden bg-blue-600/10 border border-blue-600 p-2.5 text-blue-600 flex justify-between rounded mb-5"
-      >
+        class="lg:hidden bg-blue-600/10 border border-blue-600 p-2.5 text-blue-600 flex justify-between rounded mb-5">
         <span>Swipe table to view full details</span>
-        <Icon
-          class="ml-2.5 w-5 h-5"
-          name="ic:outline-swipe"
-        />
+        <Icon class="ml-2.5 w-5 h-5" name="ic:outline-swipe" />
       </div>
-      <DataTable
-        v-if="tableStore.weeklyData?.length"
-        scrollable
-        striped-rows
-        :value="tableStore.weeklyData"
-      >
+      <DataTable v-if="tableStore.weeklyData?.length" scrollable striped-rows :value="tableStore.weeklyData">
         <Column field="index">
           <template #header="">
             <div v-tooltip.click.top="'Current Position'">
@@ -81,38 +80,21 @@ watch(selectedWeek, async (newWeek) => {
         </Column>
         <Column field="prev_week_position">
           <template #header>
-            <div
-              v-tooltip.top="'Previous Position'"
-            >
+            <div v-tooltip.top="'Previous Position'">
               Prv Pos.
             </div>
           </template>
           <template #body="slotProps">
-            <div
-              v-if="!!slotProps.data.prev_week_position"
-              class="grid grid-cols-[35px_auto]"
-            >
+            <div v-if="!!slotProps.data.prev_week_position" class="grid grid-cols-[35px_auto]">
               <div>{{ slotProps.data.prev_week_position }}</div>
               <div v-if="slotProps.data.prev_week_position < slotProps.index + 1">
-                <Icon
-                  size="16"
-                  class="text-red-500"
-                  name="flowbite:caret-down-solid"
-                />
+                <Icon size="16" class="text-red-500" name="flowbite:caret-down-solid" />
               </div>
               <div v-if="slotProps.data.prev_week_position > slotProps.index + 1">
-                <Icon
-                  size="16"
-                  class="text-green-500"
-                  name="flowbite:caret-up-solid"
-                />
+                <Icon size="16" class="text-green-500" name="flowbite:caret-up-solid" />
               </div>
               <div v-if="slotProps.data.prev_week_position === slotProps.index + 1">
-                <Icon
-                  size="20"
-                  class="text-primary"
-                  name="radix-icons:dot-filled"
-                />
+                <Icon size="20" class="text-primary" name="radix-icons:dot-filled" />
               </div>
             </div>
             <div v-else>
@@ -120,31 +102,24 @@ watch(selectedWeek, async (newWeek) => {
             </div>
           </template>
         </Column>
-        <Column
-          field="team_name"
-          header="Team"
-        >
+        <Column field="team_name" header="Team">
           <template #body="slotProps">
-            <div class="flex gap-2.5 items-center">
+            <div class="flex gap-2.5">
+              <Button title="Show points breakdown" @click="showTeamPoints($event, slotProps.data.drafted_team_id)" class="w-8 h-8 !p-1" rounded text
+                aria-label="Team information">
+                <Icon size="18" name="lucide:info" />
+              </Button>
               <div class="flex flex-col gap-1 uppercase">
                 <div class="font-black lg:text-base">
                   {{ slotProps.data.team_name }}
                 </div>
-                <div class="text-xs">
-                  {{ slotProps.data.team_owner }}
-                </div>
+                <div class="text-xs">{{ slotProps.data.team_owner }}</div>
               </div>
             </div>
           </template>
         </Column>
-        <Column
-          field="goals"
-          header="Goals"
-        />
-        <Column
-          field="assists"
-          header="Assists"
-        />
+        <Column field="goals" header="Goals" />
+        <Column field="assists" header="Assists" />
         <Column field="clean_sheets">
           <template #header>
             <div v-tooltip.top="'Total clean sheets'">
@@ -161,31 +136,20 @@ watch(selectedWeek, async (newWeek) => {
         </Column>
         <Column field="week_points">
           <template #header>
-            <div
-              v-tooltip.top="'Weekly points total'"
-              class="whitespace-nowrap"
-            >
+            <div v-tooltip.top="'Weekly points total'" class="whitespace-nowrap">
               Wk Pts.
             </div>
           </template>
           <template #body="slotProps">
             <div class="flex items-center gap-2.5">
               <div>{{ slotProps.data.week_points }}</div>
-              <Icon
-                v-if="slotProps.data.weekly_winner"
-                size="16"
-                class="text-yellow-500"
-                name="tabler:star-filled"
-              />
+              <Icon v-if="slotProps.data.weekly_winner" size="16" class="text-yellow-500" name="tabler:star-filled" />
             </div>
           </template>
         </Column>
         <Column field="total_points">
           <template #header>
-            <div
-              v-tooltip.top="'Total Points'"
-              class="whitespace-nowrap"
-            >
+            <div v-tooltip.top="'Total Points'" class="whitespace-nowrap">
               Tot Pts.
             </div>
           </template>
@@ -193,13 +157,22 @@ watch(selectedWeek, async (newWeek) => {
       </DataTable>
       <SkeletonTable v-else />
     </div>
-    <Popover ref="op">
-      <div class="flex flex-col gap-2.5">
-        <div>
-          <span class="font-medium block">Team Members</span>
+    <Dialog class="w-[90%] sm:w-[500px]" pt:header:class="!items-start !pb-2" pt:title:class="uppercase !font-black" pt:content:class="!p-0 !pb-2"
+      dismissable-mask v-model:visible="visible" modal :header="`Week ${selectedWeek} score`">
+
+      <template #header>
+        <div class="flex flex-col uppercase">
+          <span class="text-sm font-black mb-2.5 pb-2.5 border-b">Week {{ selectedWeek }} score</span>
+          <span class="text-lg font-black">{{
+            selectedDraftedTeam.team_name
+            }}</span>
+          <span class="text-xs font-light">{{
+            selectedDraftedTeam.team_owner
+            }}</span>
         </div>
-      </div>
-    </Popover>
+      </template>
+      <DraftedTeamPreviewWithPoints :active-week="selectedWeek" :drafted-team="selectedDraftedTeam" />
+    </Dialog>
     <div class="flex flex-col gap-2.5 lg:col-span-2">
       <WeeklyWinners />
     </div>
